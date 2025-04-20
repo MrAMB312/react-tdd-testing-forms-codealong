afb7581ad05f1c62d211c01554cbb63ceac37f91
Alex Blomarz <a.blomarz@gmail.com>
2025-04-20 12:03:43 -0500

    complete react-tdd-testing-forms-codealong

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -37,9 +37,52 @@ test("checkbox appears as unchecked when user clicks a second time", () => {
 });
 
 // Size select element
+test("size select element initially displays 'Small'", () => {
+  render(<App />);
+
+  const selectSize = screen.getByLabelText(/select size/i);
+
+  userEvent.selectOptions(selectSize, "medium");
+
+  expect(selectSize).toHaveDisplayValue("Medium");
+
+  userEvent.selectOptions(selectSize, "large");
+
+  expect(selectSize).toHaveDisplayValue("Large");
+});
 
 // "Your Selection" text
+test("'Your Selection' message initially displays 'small cheese'", () => {
+  render(<App />);
+
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+  const selectSize = screen.getByLabelText(/select size/i);
+
+  userEvent.click(addPepperoni);
+
+  expect(screen.getByText(/small pepperoni/i)).toBeInTheDocument();
+
+  userEvent.selectOptions(selectSize, "large");
+
+  expect(screen.getByText(/large pepperoni/i)).toBeInTheDocument();
+});
 
 // "Contact Info" text box
+test("'Contact Info' text box initially displays a placeholder value of 'email address'", () => {
+  render(<App />);
+
+  const contact = screen.getByLabelText(/enter your email address/i);
+
+  userEvent.type(contact, "pizzafan@email.com");
+
+  expect(contact).toHaveValue("pizzafan@email.com");
+});
 
 // Submit Order button
+test("form contains a 'Submit Order' button", () => {
+  render(<App />);
+
+  userEvent.click(screen.getByRole("button", { name: /submit order/i }));
+
+  expect(screen.getByText(/thanks for your order!/i)).toBeInTheDocument();
+});
